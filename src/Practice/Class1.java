fee7f2bffdfc5521faf057407934597b345b3b22
Sujata88 <sujata.kondhalkar99@gmail.com>
2022-10-18 14:25:37 +0530

    helloww

---
+++ src/Practice/Class1.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class Class1 {
+	public static void main(String[] args) {
+		
+		System.out.println("Hellow Team");
+		
+	}
+
+}
---
+++ src/Practice/Class2.java
@@ -0,0 +1,8 @@
+package Practice;
+
+public class Class2 {
+	public static void main(String[] args) {
+		System.out.println("Hellow");
+	}
+
+}
